36143b0be4bda2ebb6d3b83cb8b8328182b0389f
vengines.2010@gmail.com <vengines.2010@gmail.com>
2023-01-21 20:46:43 +0000

    added setup_cont.sh

---
+++ setup_cont.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Proxmox container Ubuntu 22.04 Setup Script add user and mod to sudo group
+# Author: VEngines Auto and Electronics
+#--------------------------------------------------------------------------------
+# Check for root to run script
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+# Setup -------------------------------------------------------------------------
+echo "Proxmox container Ubuntu 22.04 Setup Script add user and mod to sudo group"
+echo "Enter Username to add."
+read user_name
+echo
+echo "Adding username $user_name"
+adduser --gecos $user_name
+echo
+echo "Adding username $user_name to sudo"
+usermod -aG $user_name
+echo "DONE USER MODS"
+echo
+echo "Updating Server"
+apt update && apt upgrade -y
+echo
+echo "DONE UPDATING"
+echo
+echo "FINISHED SETUP"
+
