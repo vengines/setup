702a0abf2f41de31b68b3698fec301151d0c5a0a
vengines.2010@gmail.com <vengines.2010@gmail.com>
2023-01-22 19:03:47 +0000

    update script

---
+++ setup.sh
@@ -0,0 +1,21 @@
+  GNU nano 6.2                                                         setup_cont.sh                                                                  #!/usr/bin/env bash
+# shellcheck disable=SC1090
+
+# Proxmox container Ubuntu 22.04 Setup Script add user and mod to sudo group
+# Author: VEngines Auto and Electronics
+#--------------------------------------------------------------------------------
+# Check for root to run script
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+#Get setup script
+wget wget https://raw.githubusercontent.com/vengines/setup/main/setup.sh
+
+#make it exeucatible
+chmod +x setup.sh
+
+#run the script
+./setup.sh
