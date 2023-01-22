e3c4f8a7b8859ccd27dcadfd644c6c724e40e90b
vengines.2010@gmail.com <vengines.2010@gmail.com>
2023-01-22 19:43:21 +0000

    update script

--- setup_cont.sh
+++ setup_cont.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1090
 
-exec 1</dev/tty               # script-wide I/O redirection
-
 # Proxmox container Ubuntu 22.04 Setup Script add user and mod to sudo group
 # Author: VEngines Auto and Electronics
 #--------------------------------------------------------------------------------
@@ -24,23 +22,23 @@ echo
 
 #Get username to add as sudo user
 echo "Enter Username to add."
-read  user_name 
+read  user_name < /dev/tty
 echo
 # Add the username without asking for (name,room number, etc)
 echo "Adding username $user_name"
 echo
-adduser --gecos "" "$user_name"
+adduser --gecos "" "$user_name" < /dev/tty
 echo
 # Add the created user to sudo
 echo "Adding username $user_name to sudo"
-usermod -aG sudo "$user_name"
+usermod -aG sudo "$user_name" < /dev/tty
 echo
 echo "User added to sudo"
 echo
 
 # Update the server and upgrade
 echo "Updating..."
-apt update && apt upgrade -y
+apt update && apt upgrade -y < /dev/tty
 echo
 # Completed message
 echo "Done Updating.....Complete"
